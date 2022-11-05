19deff3b2e11182a2e7fb751083a0f070ab01539
ilya <ilya.0709.abvc@mail.ru>
2022-11-05 18:26:48 +0300

    FindLessThan

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
